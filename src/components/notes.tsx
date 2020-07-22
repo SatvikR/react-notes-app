d2c4768e8e65c15e9551f04caf5b2c24d12e51cb
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-07-22 10:32:53 -0700

    added loading when deleteing note

--- src/components/notes.tsx
+++ src/components/notes.tsx
@@ -22,66 +22,89 @@ interface IProps {
 }
 
 const Note: React.FC<IProps> = ({ title, _id, date }) => {
-  const deleteMethod = (_id: IProps["_id"]) => {
-    api.delete(`/api/notes/${_id}`).catch((err: Error) => console.log(err));
+  const [deleting, setDeleting] = useState<boolean>(false);
+
+  const deleteMethod = async (_id: IProps["_id"]) => {
+    setDeleting(true);
+    await api
+      .delete(`/api/notes/${_id}`)
+      .catch((err: Error) => console.log(err));
+    setDeleting(false);
   };
 
-  return (
-    <Grid celled>
-      <Grid.Row>
-        <Grid.Column width={10}>
-          <div
-            style={{ marginTop: "4%", marginBottom: "3%", marginLeft: "3%" }}
-          >
-            <Header as="h2">
-              <Icon name="file outline" /> {title}
-            </Header>
-            <p>Last Updated on: {date}</p>
-          </div>
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <div style={{ marginTop: "4%" }}>
-            <Segment textAlign="center">
-              <Popup
-                content="Edit"
-                trigger={
-                  <Button
-                    icon
-                    size="big"
-                    as={Link}
-                    to={"/edit-note/" + _id}
-                    basic
-                    color="blue"
-                  >
-                    <Icon name="edit outline" />
-                  </Button>
-                }
-              />
-              {"     "}
-              <Popup
-                content="Delete"
-                trigger={
-                  <Button
-                    icon
-                    size="big"
-                    basic
-                    color="red"
-                    onClick={() => deleteMethod(_id)}
-                  >
-                    <Icon name="trash alternate outline" />
-                  </Button>
-                }
-              />
+  if (!deleting) {
+    return (
+      <Grid celled>
+        <Grid.Row>
+          <Grid.Column width={10}>
+            <div
+              style={{ marginTop: "4%", marginBottom: "3%", marginLeft: "3%" }}
+            >
+              <Header as="h2">
+                <Icon name="file outline" /> {title}
+              </Header>
+              <p>Last Updated on: {date}</p>
+            </div>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <div style={{ marginTop: "4%" }}>
+              <Segment textAlign="center">
+                <Popup
+                  content="Edit"
+                  trigger={
+                    <Button
+                      icon
+                      size="big"
+                      as={Link}
+                      to={"/edit-note/" + _id}
+                      basic
+                      color="blue"
+                    >
+                      <Icon name="edit outline" />
+                    </Button>
+                  }
+                />
+                {"     "}
+                <Popup
+                  content="Delete"
+                  trigger={
+                    <Button
+                      icon
+                      size="big"
+                      basic
+                      color="red"
+                      onClick={() => deleteMethod(_id)}
+                    >
+                      <Icon name="trash alternate outline" />
+                    </Button>
+                  }
+                />
+              </Segment>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  } else {
+    return (
+      <Grid celled>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <Segment>
+              <Dimmer active inverted>
+                <Loader inverted content="Loading" />
+              </Dimmer>
+              <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
             </Segment>
-          </div>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  );
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 };
 
 const Notes: React.FC = () => {
-  const [notes, setNotes] = useState<Array<any> | null>(null);
+  const [notes, setNotes] = useState<Array<React.FC> | null>(null);
 
   useEffect(() => {
     if (!localStorage.getItem("uid")) {
@@ -101,6 +124,7 @@ const Notes: React.FC = () => {
         setNotes(
           currNotes.map(
             (note: { title: string; _id: string; updatedAt: string }) => {
+              const time = new Date(note.updatedAt).toLocaleTimeString();
               return (
                 <Note
                   title={note.title}
@@ -108,7 +132,7 @@ const Notes: React.FC = () => {
                   date={
                     new Date(note.updatedAt).toLocaleDateString() +
                     ", " +
-                    new Date(note.updatedAt).toLocaleTimeString()
+                    time.slice(0, time.length - 3)
                   }
                 />
               );
